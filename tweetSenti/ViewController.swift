de977c8033ee86b299023b894f62e0fa8b37de7c
Adithyah Nair <adithyah.nair@gmail.com>
2023-04-27 15:57:36 +0530

    Initial Commit

---
+++ tweetSenti/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  tweetSenti
+//
+//  Created by Adithyah Nair on 27/04/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
