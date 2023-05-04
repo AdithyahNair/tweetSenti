373dff434d93dc5a3d6ad839a518177b10e3bc3f
Adithyah Nair <adithyah.nair@gmail.com>
2023-05-04 14:27:25 +0530

    appName page created

---
+++ tweetSenti/appLauncher.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  tweetSenti
+//
+//  Created by Adithyah Nair on 27/04/23.
+//
+
+import UIKit
+
+class appLauncher: UIViewController {
+    // MARK: - Properties
+
+    @IBOutlet var lblAppName: UILabel!
+
+    override func viewDidLoad() {
+    }
+}
