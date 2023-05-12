70de88398488bead59df66968724d9254f6996b9
Adithyah Nair <adithyah.nair@gmail.com>
2023-05-12 09:41:43 +0530

    Created Past records files

---
+++ tweetSenti/Controllers/PastRecordsVCViewController.swift
@@ -0,0 +1,14 @@
+//
+//  PastRecordsVCViewController.swift
+//  tweetSenti
+//
+//  Created by Adithyah Nair on 12/05/23.
+//
+
+import UIKit
+
+class PastRecordsVCViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
