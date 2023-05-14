73a6599e46578d03b534f16516dacb4f6be1414a
Adithyah Nair <adithyah.nair@gmail.com>
2023-05-14 21:36:42 +0530

    UI testing added.

--- tweetSentiTests/tweetSentiTests.swift
+++ tweetSentiTests/tweetSentiTests.swift
@@ -5,31 +5,18 @@
 //  Created by Adithyah Nair on 14/05/23.
 //
 
+@testable import tweetSenti
 import XCTest
 
 final class tweetSentiTests: XCTestCase {
-
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+    func testsReturnEmoji() {
+        let vc = SentimentVC()
+        let score = 21
+        let emoji = vc.returnEmoji(score: score)
+        XCTAssertEqual(emoji, "😇")
     }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    
+    func testsReturndrunk() {
+        
     }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
-
 }
